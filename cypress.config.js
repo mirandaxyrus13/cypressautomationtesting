9e63184a809e2993c5beb88e0e8a9ab5023ae1de
Xyrus Miranda <xyrus.miranda@amcsgroup.com>
2022-11-07 13:00:39 +0800

    changes

--- cypress.config.js
+++ cypress.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require("cypress");
+const Mochawesome = require("mochawesome");
 
 module.exports = defineConfig({
   chromeWebSecurity: false,
@@ -11,8 +12,15 @@ module.exports = defineConfig({
   retries: {
     runMode: 1
   },
+
+  /*
+    - to record test executions via cypress dashboard, login to the cypress dashboard.
+    - create a new project, then, a projectId will be displayed. Copy it and paste it here in cypress.config.js
+    - after the configurations and settings mentioned, the dashboard is ready to record some test executions.
+  */
   projectId: "6jnhcm",
   fixturesFolder: "cypress/fixtures",
+  reporter: "mochawesome",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
